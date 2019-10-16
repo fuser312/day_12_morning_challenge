d32857b627e5fd93166e23d69aca3babf6d82b76
Sahil Singh <sahil.dogstar@gmail.com>
2019-10-16 09:17:21 +0530

    Day 12 Morning challenge completed.

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,25 @@
 /// There are a total of 10 positive numbers.
 /// The sum of all negative numbers equals -65.
 
+List countPosSumNeg(List list){
 
+  int posList = 0;
+  int negList = 0;
+  List newList = [];
+
+  for(int i = 0; i < list.length; i++){
+    if (list[i] > 0){
+      posList++;
+    }
+    else if (list[i] < 0){
+      negList = negList + list[i];
+    }
+  }
+
+    newList.add(posList);
+    newList.add(negList);
+    return newList;
+}
 
 // Challenge 2
 // The digit distance between two numbers is the absolute value of the difference of each digit.
@@ -16,9 +34,19 @@
 // Since |2 - 4| + |3 - 8| + |4 - 9| = 2 + 5 + 5
 /// Create a function that returns the digit distance between two integers.
 
+int digitDistance(int a, int b){
+  int count = 0;
+  String temp = a.toString();
+  String temp1 = b.toString();
+  for(int i = 0; i < temp.length; i++){
+    count = count + ((int.parse(temp[i]) - (int.parse(temp1[i]))))* (-1);
+  }
+  return count;
+}
 
 // Challenge 3
 // Write test cases to test previous functions
 main() {
-
+  print(countPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+  print(digitDistance(234, 489));
 }
